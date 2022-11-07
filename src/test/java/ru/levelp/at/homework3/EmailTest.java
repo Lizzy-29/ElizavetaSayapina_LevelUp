c00dc70d340f1b9c119e270ed997625454163c5a
sayapina elizaveta <sayapina@haulmont.com>
2022-11-08 03:20:27 +0400

    + правки к тестам

--- src/test/java/ru/levelp/at/homework3/EmailTest.java
+++ src/test/java/ru/levelp/at/homework3/EmailTest.java
@@ -26,7 +26,7 @@ public class EmailTest {
     }
 
     @Test
-    public void Task3() {
+    public void Task_1() {
         System.out.println("Проверка успешности авторизации");
 
         WebElement Login  = webDriver.findElement(By.id("identifierId"));
@@ -114,7 +114,7 @@ public class EmailTest {
     }
 
     @Test
-    public void Task2() {
+    public void Task_2() {
         System.out.println("Войти в почту");
         WebElement Login  = webDriver.findElement(By.id("identifierId"));
         Login.sendKeys("litavond78@gmail.com");
@@ -171,10 +171,119 @@ public class EmailTest {
                  .click();
         webDriver.findElement(By.xpath("//*[contains(text(),'Test_Test_Test')]"))
                  .isDisplayed();
+
+        System.out.println("Проверяем что контент, адресата и тему письма");
+        webDriver.findElement(By.xpath("//*[contains(text(),'Test_Test_Test')]"))
+                 .isDisplayed();
+        webDriver.findElement(By.xpath("//*[contains(text(),'Test')]"))
+                 .isDisplayed();
+        webDriver.findElement(By.xpath("//*[contains(text(),'litavond78@gmail.com')]"))
+        .isDisplayed();
+
+        System.out.println("Выйти из учётной записи");
+        webDriver.findElement(By.xpath("//a[@aria-label='Аккаунт Google: Lita Von D  \n"
+                     + "(litavond78@gmail.com)']"))
+                 .click();
+        webDriver.findElement(By.xpath("//*[contains(text(),'Выйти')]"))
+                 .isDisplayed();
+
+        webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        webDriver.findElement(By.xpath("//div//div//div//div//span//a[contains(text(),'Выйти')]"))
+                 .click();
     }
+    @Test
+    public void Task_3() {
+        System.out.println("Войти в почту");
+        WebElement Login  = webDriver.findElement(By.id("identifierId"));
+        Login.sendKeys("litavond78@gmail.com");
+
+        webDriver.findElement(By.cssSelector("button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.qIypjc.TrZEUc.lw1w4b"))
+                 .click();
+        Assertions.assertThat(webDriver.getTitle())
+                  .isEqualTo("Вход – Google Аккаунты");
+
+        webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        WebElement Password  = webDriver.findElement(By.name("Passwd"));
+        Password.sendKeys("Lita7878");
+
+        webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        WebElement Continue = webDriver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button"));
+        Continue.isEnabled();
+        Continue.click();
+
+        Assertions.assertThat(webDriver.findElement(By.cssSelector(".g3lg0e")).isEnabled());
+
+        System.out.println("Создать новое письмо");
+        webDriver.navigate().to(Email_BOX);
+        webDriver.findElement(By.xpath("//*[text()=\"Написать\"]"))
+                 .click();
+
+        webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        webDriver.findElement(By.xpath("//span[contains(text(),'Новое сообщение')]"));
+        webDriver.findElement(By.xpath("//input[@aria-label='Тема']"))
+                 .sendKeys("Test");
+        webDriver.findElement(By.xpath("//*[contains(text(),'Получатели')]"))
+                 .click();
+        webDriver.findElement(By.xpath("//*[contains(text(),'Кому')]"))
+                 .isDisplayed();
+        webDriver.findElement(By.xpath("//input[@aria-autocomplete=\"list\"]"))
+                 .sendKeys("litavond78@gmail.com");
+        webDriver.findElement(By.xpath("//div[@aria-label=\"Текст письма\"]"))
+                 .sendKeys(" Test_Test_Test");
+
+        System.out.println("Отправление письма");
+        webDriver.findElement(By.xpath("//tr//td//div//div[contains(text(),\"Отправить\")]"))
+                 .click();
+
+        System.out.println("Письмо появилось в папке Входящие");
+        webDriver.findElement(By.xpath("//*[contains(text(),'Входящие')]"))
+                 .isDisplayed();
+        webDriver.findElement(By.xpath("//*[contains(text(),'Test_Test_Test')]"))
+                 .isDisplayed();
+
+        System.out.println("Проверяем что контент, адресата и тему письма");
+        webDriver.findElement(By.xpath("//*[contains(text(),'Test_Test_Test')]"))
+                 .isDisplayed();
+        webDriver.findElement(By.xpath("//*[contains(text(),'Test')]"))
+                 .isDisplayed();
+        webDriver.findElement(By.xpath("//*[contains(text(),'litavond78@gmail.com')]"))
+                 .isDisplayed();
+
+        System.out.println("Удалить письмо");
+        webDriver.findElement(By.xpath("//div//div//div//div//div//div//div//div//div//div[@data-tooltip=\"Выбрать\"]//span"))
+                 .click();
+        webDriver.findElement(By.xpath("//body/div[7]/div[3]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]"))
+                 .click();
+
+        System.out.println("Письмо появилось в папке Корзина");
+        webDriver.findElement(By.xpath("//span[@role=\"button\"]"))
+                 .click();
+        webDriver.findElement(By.xpath("//a[contains(text(),'Корзина')]"))
+                 .click();
+        webDriver.findElement(By.xpath("//*[contains(text(),'Test_Test_Test')]"))
+                 .isDisplayed();
+
+        System.out.println("Выйти из учётной записи");
+        webDriver.findElement(By.xpath("//a[@aria-label='Аккаунт Google: Lita Von D  \n"
+                     + "(litavond78@gmail.com)']"))
+                 .click();
+        webDriver.findElement(By.xpath("//*[contains(text(),'Выйти')]"))
+                 .isDisplayed();
+
+        webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        webDriver.findElement(By.xpath("//div//div//div//div//span//a[contains(text(),'Выйти')]"))
+                 .click();
+    }
+
+
 
     @AfterMethod
     public void tearDown(){
-       webDriver.quit();
+        //webDriver.quit();
     }
 }

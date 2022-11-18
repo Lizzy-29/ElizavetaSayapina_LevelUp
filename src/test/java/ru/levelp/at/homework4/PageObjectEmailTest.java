05a1d1d5432dd15e1d378199f7ff27b5d0e43c87
sayapina elizaveta <sayapina@haulmont.com>
2022-11-18 16:13:26 +0400

    Homework6 + правки по Homework4

--- src/test/java/ru/levelp/at/homework4/PageObjectEmailTest.java
+++ src/test/java/ru/levelp/at/homework4/PageObjectEmailTest.java
@@ -31,151 +31,151 @@ public class PageObjectEmailTest {
     public void Task1(){
 
         System.out.println("Проверка успешности авторизации");
-        var AuthPage = new AuthPage(webDriver);
-        AuthPage.open();
-        AuthPage.setFillLogin();
-        AuthPage.setFillContinue();
+        var authPage = new AuthPage(webDriver);
+        authPage.open();
+        authPage.setFillLogin();
+        authPage.setFillContinue();
         Assertions.assertThat(webDriver.getTitle())
                   .isEqualTo("Вход – Google Аккаунты");
         webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        AuthPage.setFillPassword();
+        authPage.setFillPassword();
         webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        AuthPage.setContinueAuthorization();
+        authPage.setContinueAuthorization();
         Assertions.assertThat(webDriver.findElement(By.cssSelector(".g3lg0e")).isEnabled());
 
         System.out.println("Создать новое письмо (заполнить адресата, тему письма и тело)");
-        var CreateLetter = new CreateLetter(webDriver);
-        CreateLetter.open();
-        CreateLetter.newMessage();
-        CreateLetter.fillTheme();
-        CreateLetter.choiceAddress();
-        CreateLetter.choiceTo();
-        CreateLetter.fillAddress();
-        CreateLetter.textOfLetter();
+        var createLetter = new CreateLetter(webDriver);
+        createLetter.open();
+        createLetter.newMessage();
+        createLetter.fillTheme();
+        createLetter.choiceAddress();
+        createLetter.choiceTo();
+        createLetter.fillAddress();
+        createLetter.textOfLetter();
 
         System.out.println("Проверяем что письмо сохранено в черновиках");
-        CreateLetter.draft();
+        createLetter.draft();
 
         System.out.println("Проверяем контент, адресата и тему письма");
-        var Checks= new Checks(webDriver);
-        Checks.setEmail();
-        Checks.setText();
-        Checks.setTheme();
+        var checks= new Checks(webDriver);
+        checks.setEmail();
+        checks.setText();
+        checks.setTheme();
 
         System.out.println("Отправление письма");
-        CreateLetter.sendLetter();
+        createLetter.sendLetter();
 
         System.out.println("Письмо исчезло из черновиков");
-        var PackagesForLetter = new PackagesForLetter(webDriver);
-        PackagesForLetter.setDrafts();
-        PackagesForLetter.setEmptyDrafts();
+        var packagesForLetter = new PackagesForLetter(webDriver);
+        packagesForLetter.setDrafts();
+        packagesForLetter.setEmptyDrafts();
 
         System.out.println("Письмо появилось в папке отправленные");
-        PackagesForLetter.setSentLetters();
-        PackagesForLetter.setCheckSentTestLetter();
+        packagesForLetter.setSentLetters();
+        packagesForLetter.setCheckSentTestLetter();
 
         System.out.println("Выйти из учётной записи");
-        AuthPage.setLogOut();
+        authPage.setLogOut();
     }
 
     @Test
     public void Task2(){
 
         System.out.println("Проверка успешности авторизации");
-        var AuthPage = new AuthPage(webDriver);
-        AuthPage.open();
-        AuthPage.setFillLogin();
-        AuthPage.setFillContinue();
+        var authPage = new AuthPage(webDriver);
+        authPage.open();
+        authPage.setFillLogin();
+        authPage.setFillContinue();
         Assertions.assertThat(webDriver.getTitle())
                   .isEqualTo("Вход – Google Аккаунты");
         webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        AuthPage.setFillPassword();
+        authPage.setFillPassword();
         webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        AuthPage.setContinueAuthorization();
+        authPage.setContinueAuthorization();
         Assertions.assertThat(webDriver.findElement(By.cssSelector(".g3lg0e")).isEnabled());
 
         System.out.println("Создать новое письмо (заполнить адресата, тему письма и тело)");
-        var CreateLetter = new CreateLetter(webDriver);
-        CreateLetter.open();
-        CreateLetter.newMessage();
-        CreateLetter.fillTheme();
-        CreateLetter.choiceAddress();
-        CreateLetter.choiceTo();
-        CreateLetter.fillAddress();
-        CreateLetter.textOfLetter();
+        var createLetter = new CreateLetter(webDriver);
+        createLetter.open();
+        createLetter.newMessage();
+        createLetter.fillTheme();
+        createLetter.choiceAddress();
+        createLetter.choiceTo();
+        createLetter.fillAddress();
+        createLetter.textOfLetter();
 
         System.out.println("Отправление письма");
-        CreateLetter.sendLetter();
+        createLetter.sendLetter();
 
         System.out.println("Письмо появилось в папке отправленные");
-        var PackagesForLetter = new PackagesForLetter(webDriver);
-        PackagesForLetter.setSentLetters();
-        PackagesForLetter.setCheckSentTestLetter();
+        var packagesForLetter = new PackagesForLetter(webDriver);
+        packagesForLetter.setSentLetters();
+        packagesForLetter.setCheckSentTestLetter();
 
         System.out.println("Письмо появилось в папке Test");
-        var Checks = new Checks(webDriver);
-        PackagesForLetter.setTestPackage();
-        Checks.setEmail();
-        Checks.setText();
-        Checks.setTheme();
+        var checks = new Checks(webDriver);
+        packagesForLetter.setTestPackage();
+        checks.setEmail();
+        checks.setText();
+        checks.setTheme();
 
         System.out.println("Выйти из учётной записи");
-        AuthPage.setLogOut();
+        authPage.setLogOut();
     }
 
     @Test
     public void Task3(){
 
         System.out.println("Проверка успешности авторизации");
-        var AuthPage = new AuthPage(webDriver);
-        AuthPage.open();
-        AuthPage.setFillLogin();
-        AuthPage.setFillContinue();
+        var authPage = new AuthPage(webDriver);
+        authPage.open();
+        authPage.setFillLogin();
+        authPage.setFillContinue();
         Assertions.assertThat(webDriver.getTitle())
                   .isEqualTo("Вход – Google Аккаунты");
         webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        AuthPage.setFillPassword();
+        authPage.setFillPassword();
         webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        AuthPage.setContinueAuthorization();
+        authPage.setContinueAuthorization();
         Assertions.assertThat(webDriver.findElement(By.cssSelector(".g3lg0e")).isEnabled());
 
         System.out.println("Создать новое письмо (заполнить адресата, тему письма и тело)");
-        var CreateLetter = new CreateLetter(webDriver);
-        CreateLetter.open();
-        CreateLetter.newMessage();
-        CreateLetter.fillTheme();
-        CreateLetter.choiceAddress();
-        CreateLetter.choiceTo();
-        CreateLetter.fillTestAddress();
-        CreateLetter.textOfLetter();
+        var createLetter = new CreateLetter(webDriver);
+        createLetter.open();
+        createLetter.newMessage();
+        createLetter.fillTheme();
+        createLetter.choiceAddress();
+        createLetter.choiceTo();
+        createLetter.fillTestAddress();
+        createLetter.textOfLetter();
 
         System.out.println("Отправление письма");
-        CreateLetter.sendLetter();
+        createLetter.sendLetter();
 
         System.out.println("Письмо появилось в папке Входящие");
-        var PackagesForLetter = new PackagesForLetter(webDriver);
-        var Checks = new Checks(webDriver);
-        PackagesForLetter.setIncomingLetters();
+        var packagesForLetter = new PackagesForLetter(webDriver);
+        var checks = new Checks(webDriver);
+        packagesForLetter.setIncomingLetters();
         webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        Checks.setText();
+        checks.setText();
 
         System.out.println("Проверяем что контент, адресата и тему письма");
-        Checks.setText();
-        Checks.setEmail();
-        Checks.setTheme();
+        checks.setText();
+        checks.setEmail();
+        checks.setTheme();
 
         System.out.println("Удалить письмо");
-        var DeleteLetter = new DeleteLetter(webDriver);
-        DeleteLetter.setSelectLetters();
-        DeleteLetter.setDelete();
+        var deleteLetter = new DeleteLetter(webDriver);
+        deleteLetter.setSelectLetters();
+        deleteLetter.setDelete();
 
         System.out.println("Письмо появилось в папке Корзина");
-        PackagesForLetter.setToBasket();
-        PackagesForLetter.setBasket();
-        Checks.setText();
+        packagesForLetter.setToBasket();
+        packagesForLetter.setBasket();
+        checks.setText();
 
         System.out.println("Выйти из учётной записи");
-        AuthPage.setLogOut();
+        authPage.setLogOut();
     }
 
     @AfterMethod
